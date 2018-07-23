a602ab00842d0948e440d9254c5c346b14d510e0
石武浩 <shiwuhao@juzifenqi.com>
2018-07-23 18:32:29 +0800

    py

---
+++ 网络编程/demo3.py
@@ -0,0 +1,9 @@
+# /usr/bin/env python3
+from urllib.request import urlopen
+import re
+
+webpage = urlopen('http://www.python.org')
+text = webpage.read()
+
+m = re.search(b'<a href="(.*?)" (.*?)>about</a>', text, re.IGNORECASE)
+print(m.group(1))
---
+++ 网络编程/demo4.py
@@ -0,0 +1,4 @@
+# /usr/bin/env python3
+from urllib.request import urlretrieve
+
+urlretrieve('http://www.python.org', './aa.html')
